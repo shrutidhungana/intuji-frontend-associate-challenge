79628690a60618711d79936b7cf015d2b638cd18
shrutidhungana <shrutidhungana123@gmail.com>
2025-05-21 22:48:55 +0545

    Added subtle styling to project-overview: 1)Icons 2)Color etc

--- src/components/project-overview/project-overview.js
+++ src/components/project-overview/project-overview.js
@@ -1,6 +1,51 @@
+import EmptyWalletIcon from "../../icons/empty-wallet.svg";
+import ExportIcon from "../../icons/export.svg";
+import BalanceArrowIcon from "../../icons/arrow-right.svg";
 
+import SavingIcon from "../../icons/save.svg";
+import SavingExport from "../../icons/export1.svg";
+
+import ArrowIcon from "../../icons/arrow-right1.svg";
+
+import ExpenseIcon from "../../icons/expenses.svg";
+import ExpenseExport from "../../icons/export2.svg";
+
+import IncomeIcon from "../../icons/income.svg";
+import IncomeExport from "../../icons/export3.svg"
 
 export default function initProjectOverview() {
+    function injectSVG(containerId, SvgComponent) {
+      const container = document.getElementById(containerId);
+
+      if (container) {
+        container.innerHTML = "";
+        const imgElement = document.createElement("img");
+        imgElement.src = SvgComponent;
+        imgElement.alt = "Icon";
+
+        container.appendChild(imgElement);
+      }
+    }
+
+    injectSVG("wallet-icon", EmptyWalletIcon);
+    injectSVG("export-icon", ExportIcon);
+    injectSVG("balance-arrow", BalanceArrowIcon);
+
+    injectSVG("saving-icon", SavingIcon);
+    injectSVG("saving-export", SavingExport);
+    injectSVG("saving-arrow", ArrowIcon);
+
+    injectSVG("expense-icon", ExpenseIcon);
+    injectSVG("expense-export", ExpenseExport);
+    injectSVG("expense-arrow", ArrowIcon);
+
+    injectSVG("income-icon", IncomeIcon);
+    injectSVG("income-export", IncomeExport);
+    injectSVG("income-arrow", ArrowIcon);
+
+    
+    
+
   document.addEventListener("DOMContentLoaded", () => {
     // Function to generate a random number within a range
     function getRandomNumber(min, max) {
@@ -117,7 +162,8 @@ export default function initProjectOverview() {
         25000
       );
     }, 5000);
-  });
+  }
+  );
 }
 
 
