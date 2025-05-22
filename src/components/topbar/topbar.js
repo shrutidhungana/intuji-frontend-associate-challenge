c31a3bde81c4b80df3effad18f62c9e59fae4321
shrutidhungana <shrutidhungana123@gmail.com>
2025-05-22 12:52:55 +0545

    Created and completed styling of recent transaction: 1)Added styling and icons

--- src/components/topbar/topbar.js
+++ src/components/topbar/topbar.js
@@ -4,28 +4,23 @@ import MessageIcon from "../../icons/message.svg";
 import GalleryIcon from "../../icons/gallery.svg";
 import ArrowDownIcon from "../../icons/down.svg";
 
-
 export default function initTopbar() {
- 
-
   function injectSVG(containerId, SvgComponent) {
     const container = document.getElementById(containerId);
-   
+
     if (container) {
       container.innerHTML = "";
       const imgElement = document.createElement("img");
-      imgElement.src = SvgComponent; 
-      imgElement.alt = "Icon"; 
-    
+      imgElement.src = SvgComponent;
+      imgElement.alt = "Icon";
+
       container.appendChild(imgElement);
-      
     }
   }
 
-    injectSVG("search-icon", SearchIcon);
-    injectSVG("notification-icon", BellIcon);
-    injectSVG("message-icon", MessageIcon);
-    injectSVG("profile-avatar", GalleryIcon);
-    injectSVG("dropdown-icon", ArrowDownIcon);
-
+  injectSVG("search-icon", SearchIcon);
+  injectSVG("notification-icon", BellIcon);
+  injectSVG("message-icon", MessageIcon);
+  injectSVG("profile-avatar", GalleryIcon);
+  injectSVG("dropdown-icon", ArrowDownIcon);
 }
